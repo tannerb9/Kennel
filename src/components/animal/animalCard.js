26a3bb2855d9152fe5109cf917e22aa9ba0b1869
tannerb9 <tanner-brainard@hotmail.com>
2020-05-29 11:51:18 -0500

    styled to place pet name above photo and changed its spacing

--- src/components/animal/animalCard.js
+++ src/components/animal/animalCard.js
@@ -6,12 +6,12 @@ const AnimalCard = (props) => {
   return (
     <div className="card">
       <div className="card-content">
+        <h3 className="card-petname">
+          {props.animal.name}
+        </h3>
         <picture>
           <img src={require(`./${props.animal.pic}`)} alt={props.animal.name} />
         </picture>
-        <h3>
-          Name: <span className="card-petname">{props.animal.name}</span>
-        </h3>
         <p>Breed: {props.animal.breed}</p>
         <Link to={`/animals/${props.animal.id}`}>
           <button>Details</button>
